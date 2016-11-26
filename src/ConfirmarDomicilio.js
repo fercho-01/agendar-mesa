7fcc43252727e27bd1aec0c69bda70d763cd4fe8
fercho-01 <luisfernandorzcrzc@gmail.com>
2016-11-26 09:11:09 -0500

    solve

---
+++ src/ConfirmarDomicilio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import App from './App';
+import './index.css';
+import AgendarMesa from './components/agendarMesa';
+//import './Prueba';
+ReactDOM.render(
+  //<App />,
+  <AgendarMesa />,
+  document.getElementById('root2')
+);
